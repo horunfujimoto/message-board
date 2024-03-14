cc84ca2b3e6df8f77c6e6cabb3eec993082bf426
horun.fujimoto <hello.horurun@gmail.com>
2024-03-14 08:45:12 +0000

    [ADD]ページネーション

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -29,6 +29,8 @@
         </table>
     @endif
     
+    {{-- ページネーションのリンク --}}
+    {{ $messages->links() }}
     {{-- メッセージ作成ページへのリンク --}}
     <a class="btn btn-primary" href="{{ route('messages.create') }}">新規メッセージの投稿</a>
         
