5fe8212f479f637cc63f1c874508e65317840a48
horun.fujimoto <hello.horurun@gmail.com>
2024-03-14 08:34:19 +0000

    [ADD]seed追加

---
+++ database/seeders/MessagesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; // 追加するとseederを利用したデータ生成が可能
+
+class MessagesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        for($i = 1; $i <= 100; $i++) {
+            DB::table('messages')->insert([
+                'title' => 'test title ' . $i,
+                'content' => 'test content ' . $i
+            ]);
+        }
+    }
+}
