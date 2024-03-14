7db4743f33eae472989c1742f598ecdb6ae15970
horun.fujimoto <hello.horurun@gmail.com>
2024-03-14 08:41:31 +0000

    [ADD]Factoryでデータ自動生成

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    // 追記して想定外のデータが代入されるのを防ぎ、なおかつ、一気にデータを代入することが可能にする。
+    protected $fillable = ['content', 'title']; 
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Message;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Message::factory()->count(5)->create();
         $this->call([MessagesTableSeeder::class]);
     }
 }
